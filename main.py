d985df1f98b06d19f66132afb0ea5f22c1a75652
Viktor Redecker <viktor.redecker@gmail.com>
2021-10-10 16:03:20 +0200

    changed main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-""""
-Something
-"""
+"""Script"""
+
 __author__ = 'Viktor Redecker'
+
+import pandas as pd
+import sklearn
+
+class Housing:
+    def __init__(self):
+        self.df_train = pd.read_csv("data/train.csv")
+        self.df_test = pd.read_csv("data/test.csv")
